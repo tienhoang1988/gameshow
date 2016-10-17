a7f799e000387c379f952690809f6b9cc5f36929
Minh Tien Hoang <tien@tinypulse.com>
2016-10-17 23:55:01 +0700

    Update first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'game/index'
+
+  root 'game#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
